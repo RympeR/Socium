488f5fac9d245ff97d3a1c2caa4778826a6dbe8b
RympeR <42513185+RympeR@users.noreply.github.com>
2019-11-21 17:02:26 +0200

    alelua

--- Test_game/Test_game/Program.cs
+++ Test_game/Test_game/Program.cs
@@ -10,17 +10,15 @@ namespace Test_game
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             for (int i = 0; i < 15000; i++)
             {
                 Console.Write(i + " ");
             }
             Console.ReadKey();
-=======
             Console.WriteLine("check");
             Console.WriteLine("hi");
             Console.WriteLine("1");
->>>>>>> 2c44abcec863676bd87403d601289be0a1a018b1
+
         }
     }
 }
