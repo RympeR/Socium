677dc998784e2e6b228b1e5b5838a1faa125ddcd
shoza <video@shoza.com.ua>
2019-12-03 13:13:29 +0200

    Map and visual

--- Test_game/Test_game/Map.cs
+++ Test_game/Test_game/Map.cs
@@ -8,17 +8,49 @@ namespace Test_game
 {
     class Map : VisualObject
     {
+
+        int x = 20;
+        int y = 50;
+
         public Map()
         {
 
         }
 
-        public Map(String start)
+        public Map(startConditions start)
         {
+
+            if (start == startConditions.random)
+            {
+
+            }
+        }
+
+        public Map(startConditions start, int mapWidth, int mapHeight)
+        {
+            x = mapWidth;
+            y = mapHeight;
+            if (start == startConditions.random)
+            {
+
+            }
+        }
+
+        void generateRandomMap()
+        {
+            display = new char[x, y];
+
         }
 
         public override void Show()
         {
+
         }
+    }
+
+    enum startConditions
+    {
+        Default,
+        random
     }
 }
